19fb8c52ea57cad3a2cf6055d348214c479679a1
Reiley Burnitt <burnittreiley@gmail.com>
2023-10-18 17:11:20 -0600

    working scripts to run the page navigate through content tabs

--- my-portfolio/src/components/AboutMe.js
+++ my-portfolio/src/components/AboutMe.js
@@ -11,10 +11,11 @@ const AboutMe = () => {
                 <Col md={8}>
                     <h2>About Me</h2>
                     <p>
+                        My name is Reiley Burnitt and I have been coding for a little over 6 months now. I am certified in full stack web development and eager to get into the real world side of things. I just finished up with my certification with the University of Utah and am very excited to put my skills to use for whatever lucky company. I am very driven and excited to share my knowledge but also to gain knowledge from others.
 
                     </p>
                     <p>
-
+                        Thank you for your time!
                     </p>
                 </Col>
             </Row>
